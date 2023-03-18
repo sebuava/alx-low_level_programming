63dd9723919e376b3b557d08c40dd5a65386e045
Sebuava <sebuavamawuli@gmail.com>
2023-03-18 06:42:28 -0700

    print numbers six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  prints all single digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
