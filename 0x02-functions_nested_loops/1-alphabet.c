45f2bc06a0fe1630ac69363ca93e0c681532c5de
Sebuava <sebuavamawuli@gmail.com>
2023-03-22 00:21:20 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - function that prints lower case alphabets
+ * Return: Always 0;
+ */
+void print_alphabet(void)
+
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
