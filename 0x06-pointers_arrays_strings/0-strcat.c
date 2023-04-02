df5cc69591784181de77a70e51b5676a19a2ed3b
Sebuava <sebuavamawuli@gmail.com>
2023-04-02 03:39:17 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,13 +16,14 @@ char *_strcat(char *dest, char *src);
 	{
 		i++;
 	}
-	j = 0
-		while (src[j] != '\0')
-		{
-			dest[i] = src[j];
-			i++;
-			j++;
-		}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
 	dest[i] = '\0';
 	return (dest);
 }
